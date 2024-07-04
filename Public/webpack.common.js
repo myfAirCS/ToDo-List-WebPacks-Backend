5d646bcf891bce54e9c42808418c0813db1784d6
myfAirCS <younasf69@gmail.com>
2024-07-04 17:54:10 +0500

    Home in Webpacks

--- Public/webpack.common.js
+++ Public/webpack.common.js
@@ -1,58 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    entry: {
-        index:'./src/index.js',
-        login:'./src/pages/loginpage/login.js',
-        register:'./src/pages/registerpage/register.js'
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: '[name].bundle.js',
-        clean:true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                },
-                include: path.resolve(__dirname, 'src') // Use path.resolve for include paths
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                include: path.resolve(__dirname, 'src')
-            },
-            {
-                test: /\.(jpg|jpeg|gif|png|svg)$/,
-                type: 'asset/resource',
-                include: path.resolve(__dirname, 'src/assets')
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename:'index.html',
-            chunks:['index']
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/pages/loginpage/login.html'),
-            filename:'login.html',
-            chunks:['login']
-        }),
-        new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,'src/pages/registerpage/register.html'),
-            filename:'register.html',
-            chunks:['register']
-        })
-    ]
+  entry: {
+    index: "./src/index.js",
+    login: "./src/pages/loginpage/login.js",
+    register: "./src/pages/registerpage/register.js",
+    home: "./src/pages/homepage/home.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+        include: path.resolve(__dirname, "src"), // Use path.resolve for include paths
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, "src"),
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg)$/,
+        type: "asset/resource",
+        include: path.resolve(__dirname, "src/assets"),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/pages/loginpage/login.html"),
+      filename: "login.html",
+      chunks: ["login"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/pages/registerpage/register.html"),
+      filename: "register.html",
+      chunks: ["register"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/pages/homepage/home.html"),
+      filename: "home.html",
+      chunks: ["home"],
+    }),
+  ],
 };
 
 module.exports = config;
