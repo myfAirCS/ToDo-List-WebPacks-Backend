e9bc1fd5b71d32348d3723e5fd9007fe2da902fa
myfAirCS <younasf69@gmail.com>
2024-07-06 17:34:40 +0500

    Fixed Some bugs

--- Public/src/pages/homepage/home.js
+++ Public/src/pages/homepage/home.js
@@ -1,20 +1,28 @@
 import "./home.css";
-import { addTaskFunctionality } from "./utils/addTaskFunctionality.js";
-import { appendingTaskAfterUserLogsIn } from "./utils/appendingTaskAfterUserLogsIn.js.js";
-import { logoutFunction } from "./utils/logoutFunction.js";
-const addBtn = document.getElementById("add-btn");
-const logoutBtn = document.getElementById("logout-btn");
+import { getTokenFromLocalStorage } from "../../utils/getTokenFromLocalStorage.js";
+import { getTasksFromDBandAppend } from "./Functions/getTaskFromDB and Append Functionality/getTasksFromDB-AndAppend.js";
+import { userVerificationRequestUsingTokens } from "./utils/userVerification Using Tokens/userVerificationRequestUsingTokens.js";
+import { removeValuesFormLocalStorage } from "../../utils/removeValuesFromLocalStorage.js";
+import { addTaskEventListener } from "./Functions/addTask Functionality/eventListener/addTaskEventListener.js";
+import { logOutEventListener } from "./Functions/logout Funcitonality/eventListener/logOutEventListener.js";
 
+await addTaskEventListener();
+
+await logOutEventListener();
 document.addEventListener("DOMContentLoaded", async () => {
-  await appendingTaskAfterUserLogsIn();
+  const { accessToken, refreshToken } = getTokenFromLocalStorage();
+  if (!(accessToken || refreshToken)) {
+    alert("You are Not Logged In");
+    window.location.href = "login.html";
+    return;
+  }
+  await getTasksFromDBandAppend();
 });
-logoutBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (logoutFunction()) {
+window.addEventListener("load", async () => {
+  const verify = await userVerificationRequestUsingTokens();
+
+  if (!verify) {
+    removeValuesFormLocalStorage();
     window.location.href = "login.html";
   }
-});
-addBtn.addEventListener("click", async (event) => {
-  event.preventDefault();
-  await addTaskFunctionality();
 });
